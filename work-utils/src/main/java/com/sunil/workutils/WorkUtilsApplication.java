5e8665624cffa393f2b68a827d29e43d386d10cf
Sunil Kumar Sahu <sunilkumarsahu@google.com>
2018-11-16 14:56:18 +0530

    Home page screen design

---
+++ work-utils/src/main/java/com/sunil/workutils/WorkUtilsApplication.java
@@ -0,0 +1,12 @@
+package com.sunil.workutils;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkUtilsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkUtilsApplication.class, args);
+	}
+}
